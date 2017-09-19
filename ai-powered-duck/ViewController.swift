7f85e4e15cc809b2ee262746ed9b05c865850232
Vladislav Lyalin <vlyallin@gmail.com>
2017-09-19 10:10:13 +0300

    Initial Commit

---
+++ ai-powered-duck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ai-powered-duck
+//
+//  Created by Vladislav Lyalin on 19.09.17.
+//  Copyright © 2017 Vladislav Lyalin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
